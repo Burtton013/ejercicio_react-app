04485d4b0f2d03559277a2eafe6d95aad06745dc
Burtton013 <cbop95@gmail.com>
2024-01-24 13:57:21 -0600

    Cuarto commit

--- src/Components/about/about.jsx
+++ src/Components/about/about.jsx
@@ -1,5 +1,41 @@
+import "./about.css"
 const About = () => {
-    return <div>About</div>;
+    return (
+        <section>
+            <article>
+                <div>
+                    <img src="https://pbs.twimg.com/media/Fx_yAWbXsAArl3R?format=jpg&name=4096x4096" alt="Imagen de un gato en un calcetin de spiderman" />
+                </div>
+            </article>
+            <aside>
+                <h1>Carlos Oliver </h1>
+                <p>
+                    <b>Quien soy?</b>
+                </p>
+                <ul>
+                    <li>Lorem ipsum </li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                </ul>
+                <p>
+                    <b>Sigueme en mis siguientes links de contacto!</b>
+                </p>
+                <p>LinkedIn</p>
+                <p>GitHub</p>
+                <p>cbop95@gmail.com </p>
+                <section>
+                    <p>
+                        <b>Lenguajes y herramientas</b>
+                        </p>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                        </ul>
+                </section>
+            </aside>
+        </section>
+    )
+    
 };
 
 export default About;
